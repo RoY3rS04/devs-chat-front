2ed4579afebed9ace401067e83945e0e2c019328
RoYerS <rogermoises2004@gmail.com>
2023-11-16 16:51:00 -0600

    socket solved

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = import.meta.env.VITE_BACKEND_URL;
+const URL = import.meta.env.VITE_BACKEND_URL.split(':').slice(0, 2).map(i => i + ':').join('')+'3000';
 
 export const socket = io(URL, {
     extraHeaders: {
